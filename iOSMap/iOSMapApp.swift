37b599f5d840097e8257ce745411e011231675c3
杉岡成哉 <a1209s518n928@gmail.com>
2023-06-17 14:14:28 +0900

    Initial Commit

---
+++ iOSMap/iOSMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMapApp.swift
+//  iOSMap
+//
+//  Created by 杉岡成哉 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct iOSMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
