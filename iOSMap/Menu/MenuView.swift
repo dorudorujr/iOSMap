4f6c8304736fd274268ea458026bceaf80ba1bec
杉岡成哉 <a1209s518n928@gmail.com>
2023-07-02 19:37:58 +0900

    カードViewの自宅まで実装

--- iOSMap/Menu/MenuView.swift
+++ iOSMap/Menu/MenuView.swift
@@ -11,8 +11,46 @@ struct MenuView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             SearchBar(text: viewStore.binding(\.$searchText))
+            VStack {
+                VStack(spacing: .zero) {
+                    HStack {
+                        Text("よく使う項目")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("さらに表示")
+                            .font(.subheadline)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.horizontal)
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 8) {
+                            VStack {
+                                Image(systemName: "house.fill")
+                                    .renderingMode(.template)
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                    .foregroundColor(.blue)
+                                    .padding(12)
+                                    .background(Circle().fill(Color(UIColor.systemGray5)))
+                                Text("自宅")
+                                    .font(.subheadline)
+                                Text("追加")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .shadow(color: Color.black.opacity(0.2), radius: 1, x: 0, y: 1)
+                        .padding(.vertical, 5)
+                        .padding(.horizontal)
+                    }
+                }
+            }
         }
     }
 }
